8bb1857f21f7b63e2896b01cb86bb9bdb1ba8921
ananevandrei11 <ananevandrei11@gmail.com>
2026-07-23 23:09:48 +0200

    feat:implement products API - filter, review

--- backend/src/routes/products.ts
+++ backend/src/routes/products.ts
@@ -1,24 +1,57 @@
 import type { FastifyInstance } from 'fastify';
 import { prisma } from '../db';
+import type { ProductWhereInput } from '../generated/prisma/models';
 
 export async function products(fastify: FastifyInstance) {
-  fastify.get<{ Querystring: { limit?: string; offset?: string } }>(
-    '/products',
-    async (request, reply) => {
-      const { limit, offset } = request.query;
-      const take = limit !== undefined ? Number(limit) : 1000;
-      const skip = offset !== undefined ? Number(offset) : 0;
-      const products = await prisma.product.findMany({ skip, take });
-      const productTotal = await prisma.product.count();
+  fastify.get<{
+    Querystring: {
+      limit: string;
+      offset: string;
+      name?: string;
+      categoryId?: string;
+      priceMin?: string;
+      priceMax?: string;
+      discounted?: 'true';
+    };
+  }>('/products', async (request, reply) => {
+    const { limit, offset, categoryId, discounted, name, priceMax, priceMin } = request.query;
+    const take = limit !== undefined ? Number(limit) : 1000;
+    const skip = offset !== undefined ? Number(offset) : 0;
+    const filters: ProductWhereInput = {
+      ...(categoryId && { categoryId: Number(categoryId) }),
+      price: {
+        ...(priceMin && { gte: Number(priceMin) }),
+        ...(priceMax && { lte: Number(priceMax) })
+      },
+      ...(name && {
+        name: {
+          contains: name,
+          mode: 'insensitive'
+        }
+      }),
+      ...(discounted && {
+        discount: {
+          gt: 0
+        }
+      })
+    };
 
-      return reply.code(200).send({
-        limit: take,
-        offset: skip,
-        totalProducts: productTotal,
-        products
-      });
-    }
-  );
+    const products = await prisma.product.findMany({
+      where: filters,
+      skip,
+      take
+    });
+    const productTotal = await prisma.product.count({
+      where: filters
+    });
+
+    return reply.code(200).send({
+      limit: take,
+      offset: skip,
+      totalProducts: productTotal,
+      products
+    });
+  });
 
   fastify.get<{ Params: { sku: string } }>('/products/sku/:sku', async (request, reply) => {
     const sku = request.params.sku;
@@ -27,6 +60,48 @@ export async function products(fastify: FastifyInstance) {
       return reply.code(404).send('Product not found');
     }
     const review = await prisma.review.findMany({ where: { productId: product?.id } });
-    return reply.code(200).send({ ...product, review: review ?? [] });
+    return reply.code(200).send({ ...product, reviews: review ?? [] });
+  });
+
+  fastify.get('/products/get-filter', async (request, reply) => {
+    const category = await prisma.category.findMany();
+    const aggregations = await prisma.product.aggregate({
+      _max: {
+        price: true
+      },
+      _min: {
+        price: true
+      }
+    });
+    return reply.code(200).send({
+      categories: category,
+      maxPrice: aggregations._max.price,
+      minPrice: aggregations._min.price
+    });
+  });
+
+  fastify.post<{
+    Params: { sku: string };
+    Body: { name: string; rating: number; email: string; review: string };
+  }>('/products/sku/:sku/review', async (request, reply) => {
+    const sku = request.params.sku;
+    const { name, rating, review } = request.body;
+
+    const reviewData = await prisma.review.create({
+      data: {
+        productId: Number(sku),
+        description: review,
+        rating,
+        name,
+        date: new Date()
+      }
+    });
+
+    const isNewReview = !!reviewData;
+
+    return reply.code(200).send({
+      success: isNewReview,
+      message: isNewReview ? 'succes' : 'error'
+    });
   });
 }
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,16 @@
 import Fastify from 'fastify';
+import cors from '@fastify/cors';
+
 import { products } from './routes/products';
 
 const fastify = Fastify({
   logger: true
 });
 
+fastify.register(cors, {
+  origin: 'http://localhost:3000',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
+});
 fastify.get('/health', async () => ({ status: 'ok' }));
 fastify.register(products);
 
@@ -22,16 +28,19 @@ export const API = {
   productsFilter: '/products/get-filter',
   productSku: '/products/sku',
   productReview: (sku: number) => `/products/sku/${sku}/review`,
+
   order: {
     create: '/order',
     getById: (id: string) => `/order/${id}`,
     my: '/order/my'
   },
+
   auth: {
     register: '/auth/register',
     login: '/auth/login',
     restore: '/auth/restore'
   },
+
   user: {
     update: '/user/profile',
     profile: '/user/profile'
